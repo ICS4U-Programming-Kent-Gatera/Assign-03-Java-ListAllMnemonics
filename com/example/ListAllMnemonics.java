026bbc5f4cd768884519677458cf77de890b05fd
KentOCSB <92942185+KentOCSB@users.noreply.github.com>
2024-05-14 09:51:03 -0400

    Initial commit

---
+++ com/example/ListAllMnemonics.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Kent Gatera
+ * @version 1.0
+ * @since 2024-05-14
+ */
+
+// ListAllMnemonics class
+public final class ListAllMnemonics {
+
+    /** Private constructor to prevent instantiation. */
+    private ListAllMnemonics() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
